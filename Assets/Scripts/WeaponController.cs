790aef8b8d397a839f152ca0525e36a01b5df868
Michal <malpa.w.klapkach@gmail.com>
2023-09-18 21:13:45 +0200

    Add multiple weapons to WeaponController

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -1,20 +1,46 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
     public event Action<Weapon> OnWeaponChanged;
+    public event Action<Weapon> OnWeaponAdded;
 
     [SerializeField]
-    private Weapon currentWeapon;
+    private int currentWeaponIndex;
+    public Weapon CurrentWeapon => (currentWeaponIndex < 0 || currentWeaponIndex >= weapons.Count) ? null : weapons[currentWeaponIndex];
 
-    public Weapon CurrentWeapon
+    [SerializeField]
+    private List<Weapon> weapons = new List<Weapon>();
+
+    private void Update()
     {
-        get => currentWeapon;
-        set
+        if (Input.GetMouseButtonDown(0))
         {
-            currentWeapon = value;
-            OnWeaponChanged?.Invoke(currentWeapon);
+            UseWeapon();
         }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            ChangeWeapon();
+        }
+    }
+
+    public void AddWeapon(Weapon weapon)
+    {
+        weapons.Add(weapon);
+        OnWeaponAdded(weapon);
+    }
+
+    private void UseWeapon()
+    {
+        var weapon = weapons[currentWeaponIndex];
+    }
+
+    private void ChangeWeapon()
+    {
+        currentWeaponIndex++;
+        currentWeaponIndex %= weapons.Count;
+        OnWeaponChanged?.Invoke(CurrentWeapon);
     }
 }
