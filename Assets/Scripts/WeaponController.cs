9cf9c0b0c013c22ad1058fc2bd06f1d4c24e1d66
Michal <malpa.w.klapkach@gmail.com>
2023-09-18 22:17:47 +0200

    Add weapons spawning

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -8,12 +8,30 @@ public class WeaponController : MonoBehaviour
     public event Action<Weapon> OnWeaponAdded;
 
     [SerializeField]
+    private Transform weaponHolder;
+
+    [SerializeField]
     private int currentWeaponIndex;
     public Weapon CurrentWeapon => (currentWeaponIndex < 0 || currentWeaponIndex >= weapons.Count) ? null : weapons[currentWeaponIndex];
 
     [SerializeField]
     private List<Weapon> weapons = new List<Weapon>();
     public IReadOnlyList<Weapon> Weapons => weapons;
+
+    private readonly Dictionary<Weapon, GameObject> weaponObjects = new Dictionary<Weapon, GameObject>();
+
+    private void Awake()
+    {
+        foreach (var weapon in weapons)
+        {
+            var weaponObject = SpawnModel(weapon);
+            weaponObject.SetActive(false);
+            weaponObjects.Add(weapon, weaponObject);
+        }
+
+        if (CurrentWeapon != null)
+            weaponObjects[CurrentWeapon].SetActive(true);
+    }
 
     private void Update()
     {
@@ -29,7 +47,11 @@ public class WeaponController : MonoBehaviour
 
     public void AddWeapon(Weapon weapon)
     {
+        if (weapons.Contains(weapon))
+            return;
+
         weapons.Add(weapon);
+        weaponObjects.Add(weapon, SpawnModel(weapon));
         OnWeaponAdded(weapon);
     }
 
@@ -40,8 +62,18 @@ public class WeaponController : MonoBehaviour
 
     private void ChangeWeapon()
     {
+        if (CurrentWeapon != null)
+            weaponObjects[CurrentWeapon].SetActive(false);
+
         currentWeaponIndex++;
         currentWeaponIndex %= weapons.Count;
+        weaponObjects[CurrentWeapon].SetActive(true);
+        
         OnWeaponChanged?.Invoke(CurrentWeapon);
+    }
+
+    public GameObject SpawnModel(Weapon weapon)
+    {
+        return Instantiate(weapon.Model, weaponHolder);
     }
 }
