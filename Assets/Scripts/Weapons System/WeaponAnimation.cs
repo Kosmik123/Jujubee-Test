ca7820c08c5391a91858f489b7d8776155065421
Michal <malpa.w.klapkach@gmail.com>
2023-09-19 05:54:49 +0200

    Create WeaponAnimation class

---
+++ Assets/Scripts/Weapons System/WeaponAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WeaponAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Weapon weapon;
+    [SerializeField]
+    private new Animation animation;
+
+    private void OnEnable()
+    {
+        weapon.OnUsed += PlayAnimation;
+    }
+
+    [ContextMenu("Play")]
+    private void PlayAnimation()
+    {
+        if (animation.isPlaying)
+            animation.Stop();
+        animation.Play();
+    }
+
+    private void OnDisable()
+    {
+        weapon.OnUsed -= PlayAnimation;
+    }
+}
