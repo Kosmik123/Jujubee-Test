f2872fc7670dbcb5c244be6946f459ea81ac0d8c
Michal <malpa.w.klapkach@gmail.com>
2023-09-19 05:20:23 +0200

    Add pool to projectiles spawner

--- Assets/Scripts/Weapons System/ProjectilesSpawner.cs
+++ Assets/Scripts/Weapons System/ProjectilesSpawner.cs
@@ -1,13 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
+using static UnityEngine.UI.Image;
 
 public class ProjectilesSpawner : MonoBehaviour
 {
     [SerializeField]
     private Transform projectilesContainer;
 
+    private readonly Dictionary<Projectile, ObjectPool<Projectile>> poolsByPrefab = new Dictionary<Projectile, ObjectPool<Projectile>>(); 
+
     public Projectile SpawnProjectile(Projectile template, Transform origin)
     {
-        var projectile = Instantiate(template, origin.position, origin.rotation, projectilesContainer);
+        if (poolsByPrefab.TryGetValue(template, out var pool) == false)
+        {
+            pool = new ObjectPool<Projectile>(() => Instantiate(template, projectilesContainer));
+            poolsByPrefab.Add(template, pool);
+        }
+
+        var projectile = pool.Get();
+        projectile.transform.SetPositionAndRotation(origin.position, origin.rotation);
         return projectile;
     }
 }
