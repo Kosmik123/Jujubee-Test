1edabe18300ae678979f48eae96176ad4b41c4a2
Michal <malpa.w.klapkach@gmail.com>
2023-09-20 15:58:46 +0200

    Create DamagableObject class and it to enemy

---
+++ Assets/Scripts/DamagableObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DamagableObject : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+    [SerializeField]
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;    
+    }
+
+    public void Damage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0)
+            Destroy(gameObject);
+    }
+}
